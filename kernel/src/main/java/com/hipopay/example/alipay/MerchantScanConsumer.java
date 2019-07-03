474e5504f62581eb66992c771af6b065a12bac0d
kongfm <kongp3@outlook>
2019-07-03 19:02:01 +0800

    修改参数

--- kernel/src/main/java/com/hipopay/example/alipay/MerchantScanConsumer.java
+++ kernel/src/main/java/com/hipopay/example/alipay/MerchantScanConsumer.java
@@ -15,6 +15,7 @@ public class MerchantScanConsumer {
             'amount'         # 支付单金额，单位为元，精度最多小数点后两位(如果是JPY和KRW，单位为分) Y
             'currency'       # 计价币种 Y
             'product_info'   # 商品信息 Y
+            'auth_code'   # 二维码内容 Y
             'client_ip'      # 客户端设备IP地址 Y
             'notify_url'     # 异步通知地址  N
          */
@@ -24,6 +25,7 @@ public class MerchantScanConsumer {
         params.add(new BasicNameValuePair("amount", "100"));
         params.add(new BasicNameValuePair("currency", "HKD"));
         params.add(new BasicNameValuePair("product_info", "test product"));
+        params.add(new BasicNameValuePair("auth_code", "auth_code"));
         params.add(new BasicNameValuePair("client_ip", "0.0.0.0"));
         params.add(new BasicNameValuePair("notify_url", "your_notify_url"));
         //$isCNY 是否采用人民币(CNY)计价，取值"TRUE"/"FALSE"，默认值为"FALSE"
